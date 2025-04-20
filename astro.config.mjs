1b592e9e73096ec7df2597fc8368889d91fad545
fadharpra <75076391+fadharpra@users.noreply.github.com>
2025-04-20 18:44:37 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
+import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
