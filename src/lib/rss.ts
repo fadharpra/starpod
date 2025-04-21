94f34236919bc13c6b41c22cf75a478ea09672ce
Fadhli Ari Pratama <fadhli.a.pratama101@gmail.com>
2025-04-21 17:35:07 +0700

    push

--- src/lib/rss.ts
+++ src/lib/rss.ts
@@ -4,7 +4,7 @@ import { array, number, object, optional, parse, string } from 'valibot';
 
 import { dasherize } from '../utils/dasherize';
 import { truncate } from '../utils/truncate';
-import starpodConfig from '../../starpod.config';
+import { starpodConfig } from '../../config/starpod';
 
 export interface Show {
   title: string;
